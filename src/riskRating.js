ec6df81dce2594b2457c6fb162331f10e680d26c
Lharyzza <lharyzzav@missionreadyhq.com>
2026-02-25 10:45:52 +1300

    refactored code - updated riskRating, removed hardcoded items and replaced with code that returns risk rating

--- src/riskRating.js
+++ src/riskRating.js
@@ -1,25 +1,12 @@
 function calculateRiskRating(claimHistory) {
   const keyWords = ["crash", "collide", "scratch", "bump", "smash"];
 
-  // let riskRating = 0;
-
   if (typeof claimHistory !== "string") {
     throw new Error("Error: invalid input - must be string");
   }
 
   if (!claimHistory.trim()) {
     return null;
-  }
-
-  // if (claimHistory === "crash collide smash crash") {
-  //   return 4;
-  // }
-
-  // if (claimHistory === "crash") {
-  //   return 1;
-  // }
-  if (claimHistory === "crash crash smash collide scratch bump scratch") {
-    return 5;
   }
 
   const numberOfKeyWords = claimHistory
