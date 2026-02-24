3aeb6299407acbaea0320202653c9fed0d1ae324
Lharyzza <lharyzzav@missionreadyhq.com>
2026-02-25 10:56:03 +1300

    added API riskRating and updated error status

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
 const { calculateRiskRating } = require("./src/riskRating");
 
 const app = express();
+app.use(express.json());
 dotenv.config();
 
 app.post("/riskRating", (req, res) => {
-  res.json({
-    keywords: _.sample(keywords),
-  });
+  const { claimHistory } = req.body;
+
+  if (claimHistory === undefined) {
+    return res.status(400).json({ error: "Error! please enter claim History" });
+  }
+
+  if (typeof claimHistory !== "string") {
+    return res
+      .status(400)
+      .json({ error: "Error! Claim History must be a string!" });
+  }
 });
 
 app.post("/riskRating", () => {});
