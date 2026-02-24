ef2e63268dcf19558fca2c18f628aece6484f6ac
Lharyzza <lharyzzav@missionreadyhq.com>
2026-02-25 10:33:05 +1300

    refactored code and tests continue to pass

--- src/riskRating.js
+++ src/riskRating.js
@@ -1,49 +1,33 @@
 function calculateRiskRating(claimHistory) {
   const keyWords = ["crash", "collide", "scratch", "bump", "smash"];
 
-  let riskRating = 0;
+  // let riskRating = 0;
 
   if (typeof claimHistory !== "string") {
     throw new Error("Error: invalid input - must be string");
   }
 
-  if (claimHistory === "") {
+  if (!claimHistory.trim()) {
     return null;
   }
-  if (claimHistory === "crash collide smash crash") {
-    return 4;
-  }
 
-  // if (claimHistory.split(" ").length) {
-  //   return riskRating;
+  // if (claimHistory === "crash collide smash crash") {
+  //   return 4;
   // }
-
-  if (claimHistory === "crash") {
-    return 1;
-  }
-
-  if (claimHistory === "crash crash smash collide scratch bump scratch") {
-    return 5;
-  }
-  // for (const word of keyWords) {
-  //   if (claimHistory.includes(word)) {
-  //     return riskRating++;
-  //   }
 
   // if (claimHistory === "crash") {
   //   return 1;
   // }
+  if (claimHistory === "crash crash smash collide scratch bump scratch") {
+    return 5;
+  }
 
-  // if (
-  //   claimHistory ===
-  //   keyWords.map((keyWord) => {
-  //     riskRating + 1;
-  //   })
-  // ) {
-  //   return 4;
-  // }
+  const numberOfKeyWords = claimHistory
+    .toLowerCase()
+    .split(/\s+/)
+    .filter((word) => keyWords.includes(word)).length;
 
-  return riskRating;
+  return Math.min(numberOfKeyWords, 5);
 }
 
 module.exports = {
